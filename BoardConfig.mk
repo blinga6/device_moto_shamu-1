b4007440f67a9ae39c243a80291353af9c3a6f50
despairfactor <matthewdalex92@gmail.com>
2015-10-12 21:09:09 -0400

    Specify toolchain and kernel source for Despair Kernel
    # Conflicts:
    #	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,17 +23,14 @@ TARGET_CPU_VARIANT := krait
 ENABLE_CPUSETS := true
 
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_BOOTLOADER := true
+ 
 # Inline kernel building
-TARGET_KERNEL_CONFIG := shamu_defconfig
-TARGET_KERNEL_SOURCE := kernel/moto/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
-
-# Inline kernel building
-TARGET_KERNEL_CONFIG := shamu_defconfig
+TARGET_GCC_VERSION_EXP := 4.9
+TARGET_GCC_VERSION_ARM := 4.9
+TARGET_KERNEL_CONFIG := despair_defconfig
 TARGET_KERNEL_SOURCE := kernel/moto/shamu
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  2048
